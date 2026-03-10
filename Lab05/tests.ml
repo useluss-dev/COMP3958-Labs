8feb778a3d926f97df3160c5e3c1b6409dc73d5a
Ryan Fiset <ryfiset@gmail.com>
2026-03-10 13:50:30 -0700

    Update

--- Lab05/tests.ml
+++ Lab05/tests.ml
@@ -6,21 +6,28 @@
    #use "tests.ml";;
    run ();;
  *)
-module M = Kvtree.Make(Int);;
+module M = Kvtree.Make (Int)
 
-let t = M.of_list [(3, "three"); (2, "two"); (7, "seven"); (6, "six"); (8, "eight")]
-let l = [(2, "two"); (3, "three"); (6, "six"); (7, "seven"); (8, "eight")]
-let s = 
-  "^(3, three, ^(2, two, #, #), ^(7, seven, ^(6, six, #, #), ^(8, eight, #, #)))"
+let t =
+  M.of_list [ (3, "three"); (2, "two"); (7, "seven"); (6, "six"); (8, "eight") ]
 
-let t2 = M.of_list [(3, 'a')]
+let l = [ (2, "two"); (3, "three"); (6, "six"); (7, "seven"); (8, "eight") ]
+
+let s =
+  "^(3, three, ^(2, two, #, #), ^(7, seven, ^(6, six, #, #), ^(8, eight, #, \
+   #)))"
+
+let t2 = M.of_list [ (3, 'a') ]
 let s2 = "^(3, a, #, #)"
-
 let t3 = M.insert 2 'b' t2
 let s3 = "^(3, a, ^(2, b, #, #), #)"
 
-let t4 = M.of_list [(3, "3"); (7, "7"); (5, "5"); (6, "6"); (8, "8"); (9, "9")] 
-let s4 = "^(3, 3, #, ^(7, 7, ^(5, 5, #, ^(6, 6, #, #)), ^(8, 8, #, ^(9, 9, #, #))))"
+let t4 =
+  M.of_list [ (3, "3"); (7, "7"); (5, "5"); (6, "6"); (8, "8"); (9, "9") ]
+
+let s4 =
+  "^(3, 3, #, ^(7, 7, ^(5, 5, #, ^(6, 6, #, #)), ^(8, 8, #, ^(9, 9, #, #))))"
+
 let t5 = M.delete 7 t4
 let s5a = "^(3, 3, #, ^(6, 6, ^(5, 5, #, #), ^(8, 8, #, ^(9, 9, #, #))))"
 let s5b = "^(3, 3, #, ^(8, 8, ^(5, 5, #, ^(6, 6, #, #)), ^(9, 9, #, #)))"
---
+++ Lab06/primes.ml
@@ -0,0 +1,21 @@
+type 'a infstream = Cons of 'a * (unit -> 'a infstream)
+
+(** [from n] creates a infinite stream of integers starting at [n]. **)
+let rec from n = Cons (n, fun () -> from (n + 1))
+
+(** [take n s] creates a list of the first [n] elements of a given infinite
+    stream [s]. **)
+let rec take n (Cons (h, t)) = if n <= 0 then [] else h :: take (n - 1) (t ())
+
+(** [filter f s] filters out all elements in a given infinite stream [s] that
+    don't satisfy the predicate [f]. *)
+let rec filter f (Cons (h, t)) =
+  if f h then Cons (h, fun () -> filter f (t ())) else filter f (t ())
+
+(** [sieve s] removes all values in [s] who is a multiple of [h]. This is a
+    method for finding prime numbers. *)
+let rec sieve (Cons (h, t)) =
+  Cons (h, fun () -> sieve @@ filter (fun x -> x mod h <> 0) (t ()))
+
+(** [generate_primes] generates an infinite stream of all primes. *)
+let generate_primes = sieve (from 2)
